d955d1971d2344ac7074369c65623554456cc9a7
Thomas Bender <thomas.bender82@qmail.cuny.edu>
2021-11-22 16:39:29 -0500

    add nginx conf

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -x
 
+sudo apt install -y python nginx
+# ubuntu 20 removed python2, python2 necessary for npm sqlite3 4.2.0. 
+# npm sqlite3 4.2.0 used to due to sqlite3 5.0.2 having a security warning
+
 touch ~/.bashrc
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 source ~/.bashrc
@@ -9,6 +13,11 @@ cd urlshortener
 
 nvm install --lts
 npm install
+
+sudo unlink /etc/nginx/sites-enabled/default
+sudo cp ./scripts/urlshortener.conf /etc/nginx/sites-available/
+sudo ln -s /etc/nginx/sites-available/reverse-proxy.conf /etc/nginx/sites-enabled/reverse-proxy.conf
+sudo service nginx restart
 
 sudo cp ./scripts/urlshortener.service /lib/systemd/system
 sudo systemctl daemon-reload
