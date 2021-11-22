b33511517b5860b473ef0b5dc9d00b5b8af76fff
Thomas Bender <thomas.bender82@qmail.cuny.edu>
2021-11-22 18:39:03 -0500

    removed extra comments

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,8 +3,6 @@
 # expect script with current directory in urlshortener/scripts
 sudo systemctl stop urlshortener
 cd /home/tbender/urlshortener/
-# kill systemd service
 git pull
 npm update
-# restart systemd service
 sudo systemctl start urlshortener
